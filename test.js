9fe7de0d35ad6908c5a78d35ed39007a90d18537
Dimitri9988 <dimitri.steiner@ict.csbe.ch>
2023-11-30 12:05:34 +0100

    Reservations versuch

--- test.js
+++ test.js
@@ -1,2 +1,12 @@
-const Number = 33
-const Number2 = 44
+const Number = {
+    num1:17, 
+    num2: 20}
+const Number2 = {
+    num1:14, 
+    num2: 17}
+
+// Schut ob Reservqation wsich überschneiden oder pb die ausgewählte zeit verfügbar ist
+if (Number.num1 <= Number2.num1 && Number.num1 <= Number2.num2 && Number.num2 <= Number2.num1 && Number.num2 <= Number2.num2 || Number.num1 >= Number2.num1 && Number.num1 >= Number2.num2 && Number.num2 >= Number2.num1 && Number.num2 >= Number2.num2) {
+    console.log("Can resevate")
+}
+    
