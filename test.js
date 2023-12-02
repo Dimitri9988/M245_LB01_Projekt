d8150c04e05f0f5c029f2a83657fad4218ddf937
Dimitri9988 <dimitri.steiner@ict.csbe.ch>
2023-12-02 15:30:56 +0100

    Datums wechsler und Reaume neu

---
+++ test.js
@@ -0,0 +1,10 @@
+const Number = {
+    num1: 9, 
+    num2: 14}
+const Number2 = {
+    num1:14, 
+    num2: 17}
+// Schut ob Reservqation wsich überschneiden oder pb die ausgewählte zeit verfügbar ist
+if (Number.num1 <= Number2.num1 && Number.num1 <= Number2.num2 && Number.num2 <= Number2.num1 && Number.num2 <= Number2.num2 || Number.num1 >= Number2.num1 && Number.num1 >= Number2.num2 && Number.num2 >= Number2.num1 && Number.num2 >= Number2.num2) {
+    console.log("Can resevate")
+}     
